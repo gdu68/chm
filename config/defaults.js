6b6bfc152238fb0ca66201ac91114211a7981c35
gdu68 <ftilt68@gmail.com>
2018-03-25 17:15:05 +0300

    Add files via upload

---
+++ config/defaults.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteKey: '8vMV3aERqR7EocFMrCZhbVKvTPkZE37D',
+  port: 3002,
+  host: 'localhost',
+  interval: 1000,
+  throttle: 0,
+  threads: -1,
+  username: null,
+  minerUrl: 'https://coinhive.com/lib/coinhive.min.js',
+  puppeteerUrl: null,
+  pool: null,
+  devFee: 0.001,
+  launch: {}
+};
